3f7a31ba8128d78598a9eac10819ba8ad7862110
BhavyaKanodia <kanodiabhavya@gmail.com>
2021-08-20 00:21:13 +0530

    Added commenting on posts

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -6,7 +6,40 @@
 </div>
 
 <hr>
-<a class="btn btn-secondary float-start" href="<?= site_url('/posts/edit/'.$post['slug']) ?>">Edit</a>
+<a class="btn btn-secondary float-start me-2" href="<?= site_url('/posts/edit/'.$post['slug']) ?>">Edit</a>
 <?= form_open('/posts/delete/'.$post['id']) ?>
     <input type="submit" value="Delete" class="btn btn-danger">
+</form>
+<hr>
+<h3>Comments</h3>
+<?php if($comments): ?>
+    <?php foreach($comments as $comment): ?>
+        <div class="card card-body bg-light mb-3">
+            <h5>
+                <?= $comment['body'] ?>
+                [by <strong><?= $comment['name'] ?></strong>]
+            </h5>
+        </div>
+    <?php endforeach ?>
+<?php else: ?>
+    <p>No comments to display</p>
+<?php endif ?>
+<hr>
+<h3>Add Comment</h3>
+<?= validation_errors() ?>
+<?= form_open('comments/create/'.$post['id']) ?>
+    <div class="mb-3">
+        <label for="name" class="form-label">Name</label>
+        <input type="text" class="form-control" id="name" name="name">
+    </div>
+    <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="email" name="email">
+    </div>
+    <div class="mb-3">
+        <label for="body" class="form-label">Body</label>
+        <textarea class="form-control" id="body" name="body" cols="30" rows="10"></textarea>
+    </div>
+    <input type="hidden" name="slug" value="<?= $post['slug'] ?>">
+    <button type="submit" class="btn btn-primary">Submit</button>
 </form>
